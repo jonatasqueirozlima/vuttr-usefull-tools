993cd922f5a2009d566a929094581994ea2fbeb8
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-28 11:09:05 -0300

    feat: :sparkles: add Chakra Wrapper

---
+++ lib/providers/ChakraWrapper.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ChakraWrapper({ children }: Props) {
+  return <ChakraProvider>{children}</ChakraProvider>;
+}
