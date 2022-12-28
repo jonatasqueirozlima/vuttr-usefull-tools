fe7293c4392d7e862d1bf77a0099728a8d41351d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-28 11:07:13 -0300

    feat: :label: add tool types

---
+++ lib/types/index.d.ts
@@ -0,0 +1,20 @@
+import type toolSchema from 'lib/schemas/toolSchema';
+
+export interface Tool {
+  id?: number;
+  title: string;
+  link: string;
+  description: string;
+  tags: string[] | string;
+}
+
+export interface ToolsState {
+  tools: Tool[] | [];
+  addTool: (tool: Tool) => Promise<void>;
+  removeTool: (id: number) => Promise<void>;
+  fetchTools: () => Promise<void>;
+  getByTextTool: (text: string) => Promise<void>;
+  getByTagTool: (tag: string) => Promise<void>;
+}
+
+export type ToolSchema = z.infer<typeof toolSchema>;
