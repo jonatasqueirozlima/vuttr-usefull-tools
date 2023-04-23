0c002b6bad79a6eb81a3b8f13bc07227c4c93b5f
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-04-23 18:14:59 -0300

    feat: :sparkles: persist data
    with local storage

--- lib/store/useToolsStore.ts
+++ lib/store/useToolsStore.ts
@@ -1,61 +1,73 @@
 import type { Tool, ToolsState } from "lib/types";
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 const API_URL = "https://vuttr-api-server.vercel.app";
+
 const headers = {
   "Content-Type": "application/json",
 };
 
-const useToolsStore = create<ToolsState>((set) => ({
-  tools: [],
-  tagOnly: false,
-  handlerTagOnly: (checked: boolean) =>
-    set((state) => ({ ...state, tagOnly: checked })),
-  addTool: async (tool) => {
-    const response = await fetch(`${API_URL}/tools`, {
-      method: "POST",
-      body: JSON.stringify(tool),
-      ...headers,
-    });
-    const data = (await response.json()) as Tool;
+const useToolsStore = create<ToolsState, [["zustand/persist", unknown]]>(
+  persist(
+    (set) => ({
+      tools: [],
+      tagOnly: false,
+      handlerTagOnly: (checked: boolean) =>
+        set((state) => ({ ...state, tagOnly: checked })),
+      addTool: async (tool) => {
+        const response = await fetch(`${API_URL}/tools`, {
+          method: "POST",
+          body: JSON.stringify(tool),
+          ...headers,
+        });
+        const data = (await response.json()) as Tool;
 
-    set((state) => ({ tools: [...state.tools, data] }));
-  },
-  removeTool: async (id: number) => {
-    await fetch(`${API_URL}/tools/${id}`, {
-      method: "DELETE",
-      ...headers,
-    });
-    set((state) => ({ tools: state.tools.filter((tool) => tool.id !== id) }));
-  },
-  fetchTools: async () => {
-    const response = await fetch(`${API_URL}/tools`, {
-      method: "GET",
-      ...headers,
-    });
+        set((state) => ({ tools: [...state.tools, data] }));
+      },
+      removeTool: async (id: number) => {
+        await fetch(`${API_URL}/tools/${id}`, {
+          method: "DELETE",
+          ...headers,
+        });
+        set((state) => ({
+          tools: state.tools.filter((tool) => tool.id !== id),
+        }));
+      },
+      fetchTools: async () => {
+        const response = await fetch(`${API_URL}/tools`, {
+          method: "GET",
+          ...headers,
+        });
 
-    const data = (await response.json()) as Tool[];
+        const data = (await response.json()) as Tool[];
 
-    set({ tools: data });
-  },
-  getByTextTool: async (text: string) => {
-    const response = await fetch(`${API_URL}/tools?q=${text}`, {
-      method: "GET",
-      ...headers,
-    });
-    const data = (await response.json()) as Tool[];
+        set({ tools: data });
+      },
+      getByTextTool: async (text: string) => {
+        const response = await fetch(`${API_URL}/tools?q=${text}`, {
+          method: "GET",
+          ...headers,
+        });
+        const data = (await response.json()) as Tool[];
 
-    set({ tools: data });
-  },
-  getByTagTool: async (tag: string) => {
-    const response = await fetch(`${API_URL}/tools?tags_like=${tag}`, {
-      method: "GET",
-      ...headers,
-    });
-    const data = (await response.json()) as Tool[];
+        set({ tools: data });
+      },
+      getByTagTool: async (tag: string) => {
+        const response = await fetch(`${API_URL}/tools?tags_like=${tag}`, {
+          method: "GET",
+          ...headers,
+        });
+        const data = (await response.json()) as Tool[];
 
-    set({ tools: data });
-  },
-}));
+        set({ tools: data });
+      },
+    }),
+    {
+      name: "tools-storage",
+      partialize: (state) => ({ tools: state.tools }),
+    }
+  )
+);
 
 export default useToolsStore;
